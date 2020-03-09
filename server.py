82ba5337120108ba9ac09ea52897e78c0aad8b46
guffre <mtguffr@gmail.com>
2020-03-08 20:46:48 -0400

    Added comments, slight JS refactor

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 from flask import Flask
 from flask import render_template
+from flask import send_from_directory
 from flask import jsonify
 
 import sqlite3
@@ -9,14 +10,17 @@ import sys
 
 app = Flask(__name__)
 
+# This is the homepage, so the default page displayed
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/index.js")
-def index_scripts():
-    return render_template("static/js/index.js")
+# This serves javascript files from the static/js directory
+@app.route('/static/js/<path:path>')
+def send_js(path):
+    return send_from_directory('static/js', path)
 
+# Serves the top-ten most common passwords, retrieved from the database
 @app.route("/top_ten.php")
 def top_ten():
 	db = sqlite3.connect('/var/log/passwords.db')
@@ -24,6 +28,7 @@ def top_ten():
 	data = cursor.execute('SELECT password,COUNT(*) from passwords group by password order by COUNT(*) DESC LIMIT 10;')
 	return jsonify({"data": [["password","count"]] + data.fetchall()})
 
+# Runs the webserver
 if __name__ == "__main__":
     run_port = int(sys.argv[-1])
     app.run(host="0.0.0.0", port=run_port)
--- static/js/index.js
+++ static/js/index.js
@@ -1,25 +1,30 @@
-
-function get_latest() {
+// This function will retrieve the most recent passwords added to the database file
+// It will then update the div with id "latest-attempts-box"
+function get_latest(){
     var latest_attempts = $.ajax({
         type: "GET",
         dataType: "html",
         url: "latest.php",
-    }).complete(function () {
-        setTimeout(function () { get_latest(); }, 2000);
-    }).responseText;
+        complete: function(){ setTimeout(function(){get_latest();}, 5000); }
+    });
     $('div.latest-attempts-box').html(latest_attempts);
 };
 
-function get_top_ten() {
+// This function gets the top ten most common seen passwords from the database file
+// It will return this data as a string
+function get_top_ten(){
     var top_ten = $.ajax({
         type: "GET",
         dataType: "json",
         url: "top_ten.php",
         async: false
     }).responseText;
+    return top_ten;
 };
 
-function get_stats() {
+// This function will retrieve various statistics about the password collection
+// It will then update the div with id "stats-box"
+function get_stats(){
     var stats = $.ajax({
         type: "GET",
         dataType: "html",
@@ -27,3 +32,8 @@ function get_stats() {
     }).responseText;
     $('div.stats-box').html(stats);
 };
+
+$(document).ready(function(){
+    new get_latest();
+    new get_stats();
+});
